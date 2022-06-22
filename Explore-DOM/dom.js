b0534bfc26b1c94c5ea5cec47a1903ed86291cac
hasan <mdhasanhk.98@gmail.com>
2022-06-22 12:20:39 -0400

    Dom

--- Explore-DOM/dom.js
+++ Explore-DOM/dom.js
@@ -8,6 +8,7 @@ for(i = 0; i < author.length; i++){
     var element = author[i];
     console.log(element.innerHTML)
     element.innerHTML = 'lekhok-'+ [i + 1];
+    element.setAttribute('title', 'this is attribute')
     var color = element.style.color="red"
 }
 
@@ -16,3 +17,8 @@ for(i = 0; i < queary.length; i++){
     var element = queary[i];
     element.innerHTML = 'hasan-' + [i + 1];
 }
+
+const article = document.getElementById('first');
+const newP = document.createElement('p');
+newP.innerHTML = "this is new ph"
+article.appendChild(newP);
